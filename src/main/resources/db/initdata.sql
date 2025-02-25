9e5f103200f99a7378a43ed87d3c09bbf6439836
nahyun <knahyuk@gmail.com>
2025-02-25 13:36:15 +0900

    초기 데이터 추가(피처 테스트용)

---
+++ src/main/resources/db/initdata.sql
@@ -0,0 +1,25 @@
+INSERT INTO post (title, content, deleted) VALUES ('첫 번째 게시글', '본문1', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('두 번째 게시글', '본문2', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 3', '본문3', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 4', '본문4', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 5', '본문5', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 6', '본문6', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 7', '본문7', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 8', '본문8', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 9', '본문9', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 10', '본문10', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 11', '본문11', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 12', '본문12', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 13', '본문13', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 14', '본문14', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 15', '본문15', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 16', '본문16', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 17', '본문17', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 18', '본문18', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 19', '본문19', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 20', '본문20', FALSE);
+INSERT INTO post (title, content, deleted) VALUES ('게시글 21', '본문21', FALSE);
+
+INSERT INTO comment (post_id, content, deleted) VALUES (1, '첫 번째 댓글', FALSE);
+INSERT INTO comment (post_id, content, deleted) VALUES (1, '두 번째 댓글', FALSE);
+INSERT INTO comment (post_id, content, deleted) VALUES (2, '세 번째 댓글', FALSE);
