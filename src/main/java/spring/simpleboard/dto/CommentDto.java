0427848dc4dbdb18e07596b8c47bc2f5303a5b2c
nahyun <knahyuk@gmail.com>
2025-02-27 02:40:09 +0900

    added Validation annotation to dto

--- src/main/java/spring/simpleboard/dto/CommentDto.java
+++ src/main/java/spring/simpleboard/dto/CommentDto.java
@@ -1,13 +1,18 @@
 package spring.simpleboard.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 import spring.simpleboard.entity.Comment;
 
 @Getter
 @AllArgsConstructor
 public class CommentDto {
     private Long id;
+
+    @NotBlank(message = "{comment.content.notBlank}")
+    @Length(max = 200, message = "{comment.content.length}")
     private String content;
 
     public static CommentDto from(Comment comment) {
--- src/main/java/spring/simpleboard/dto/PostDto.java
+++ src/main/java/spring/simpleboard/dto/PostDto.java
@@ -1,20 +1,33 @@
 package spring.simpleboard.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.annotation.Validated;
 import spring.simpleboard.entity.Post;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostDto {
     private Long id;
+
+    @NotBlank(message = "{post.title.notBlank}")
+    @Length(max = 50, message = "{post.title.length}")
     private String title;
+
+    @NotBlank(message = "{post.content.notBlank}")
     private String content;
+
+    @Valid
     private List<CommentDto> comments;
 
     public static PostDto from (Post post, List<CommentDto> comments) {
